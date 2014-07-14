726580f09475be9b98dfc0238f33e69a96f7eb2b
sarunyoo wangwong <jing@sarunyoos-MacBook-Pro.local>
2014-07-14 12:40:04 +0700

    use config after do alias

--- lib/extmodules/form_field.rb
+++ lib/extmodules/form_field.rb
@@ -6,24 +6,24 @@ module FormField
       define_method :prepare_config do
         mod_field rescue "skip"
         unless @config[:id].nil?
-          @default_config.merge! :submitValue => true
-          @default_config.merge! :name => conv_id_to_name
+          @config.merge! :submitValue => true
+          @config.merge! :name => conv_id_to_name
 
           if @xtype == 'textfield'
-            @default_config.merge! :emptyText => conv_id_to_label.capitalize
+            @config.merge! :emptyText => conv_id_to_label.capitalize
           end
 
           if base.to_s.match /Checkbox$|Radio$|Radiogroup$/
-            @default_config.merge! :boxLabel => conv_id_to_label.capitalize
+            @config.merge! :boxLabel => conv_id_to_label.capitalize
           end
           # if base.to_s.match /Combo$/
             # user hidden name for combobox
-            #@default_config.merge! :name => nil #, :hiddenName => conv_id_to_name
+            #@config.merge! :name => nil #, :hiddenName => conv_id_to_name
           # end
           unless base.to_s.match /Checkbox$|Radio$/
-            @default_config.merge! :fieldLabel => conv_id_to_label.capitalize
-          #   label_width = @default_config[:fieldLabel].size * ExtUtil.FontWidthRatio + 15
-          #   @default_config.merge! :labelWidth => label_width
+            @config.merge! :fieldLabel => conv_id_to_label.capitalize
+          #   label_width = @config[:fieldLabel].size * ExtUtil.FontWidthRatio + 15
+          #   @config.merge! :labelWidth => label_width
           end
         end
       end
